cd7d27bb00aa593205f05fa4ba0df11355754458
CesarCasasola <cecm2893@gmail.com>
2020-01-31 04:11:49 -0600

    Adding Slim 3 examples

--- api/category/index.php
+++ api/category/index.php
@@ -18,11 +18,36 @@ $app = new \Slim\App([
 $container = $app->getContainer();
 
 $container["db"] = $db;
+$container["post"] = new Post($db);
 	
-$app->get('/hello/{name}', function ($request, $response) {
+$app->get('/hello[/{name}]', function ($request, $response, $args) {
+    //Squared brackets makes an attribute to be optional
+    var_dump($args);
+    //Attribute:  /something/attr1/attr2 ...
     echo "Hello ".$request->getAttribute('name');
-    print_r($this->db);
-    
+    // hello/{name}?page=5 page -> param
+    echo $request->getParam('page');
+    print_r($this->db);    
+});
+
+$app->post('/create', function($request, $response){
+    $this->post->title = $request->getParam("title");
+    $this->post->body = $request->getParam("body");
+    $this->post->author = $request->getParam("author");
+    $this->post->category_id = $request->getParam("category_id");
+    if($this->post->create()){
+        echo json_encode(
+            array(
+                "message" => "Post created",
+            )
+        );
+    }   else{
+        echo json_encode(
+            array(
+                "message" => "Post not created",
+            )
+        );
+    }
 });
 
 $app->run();
